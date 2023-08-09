13fa681359d9d124fae696dbc9c878b85d2adf4a
jhkim <wellsy001012@gmail.com>
2023-08-09 16:46:56 +0900

    tokenProvider작성

--- backend/src/main/java/onboarding/wanted/backend/global/auth/TokenProvider.java
+++ backend/src/main/java/onboarding/wanted/backend/global/auth/TokenProvider.java
@@ -1,29 +1,102 @@
 package onboarding.wanted.backend.global.auth;
 
+import io.jsonwebtoken.*;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.InitializingBean;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Component;
 
 import java.security.Key;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Date;
+import java.util.stream.Collectors;
 
 @Component
+@RequiredArgsConstructor
 public class TokenProvider implements InitializingBean {
 
     private final Logger logger = LoggerFactory.getLogger(TokenProvider.class);
     private static final String AUTHORITIES_KEY = "auth";
-//    private final String secret;
-//    private final long tokenValidityInMilliseconds;
+
+    @Value("${jwt.secret}")
+    private String secret; //TODO: autowire
+
+    @Value("${jwt.access-token-validity-seconds}")
+    private long tokenValiditySeconds;
     private Key key;
 
-    public TokenProvider(
-
-    ) {
-
-    }
-
+    //HMAC key설정
     @Override
     public void afterPropertiesSet() throws Exception {
+        byte[] keyBytes = Decoders.BASE64.decode(secret);
+        this.key = Keys.hmacShaKeyFor(keyBytes);
+    }
 
+    //권한정보를 가지고 token생성
+    public String createToken(Authentication authentication) {
+        String authorities = authentication.getAuthorities().stream()
+                .map(GrantedAuthority::getAuthority)
+                .collect(Collectors.joining(","));
+
+        long now = (new Date()).getTime();
+        Date expiration = new Date(now + this.tokenValiditySeconds * 1000);
+
+        return Jwts.builder()
+                .setSubject(authentication.getName())
+                .claim(AUTHORITIES_KEY, authorities)
+                .signWith(key, SignatureAlgorithm.HS512)
+                .setExpiration(expiration)
+                .compact();
+    }
+
+    // token의 정보로부터 Authentication 객체 리턴
+    public Authentication getAuthentication(String token) {
+
+        //토큰으로부터 claim 파싱
+        Claims claims = Jwts
+                .parserBuilder()
+                .setSigningKey(key)
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+
+        //토큰의 "auth"클레임으로부터 권한 객체 생성
+        Collection<? extends GrantedAuthority> authorities =
+                Arrays.stream(claims.get(AUTHORITIES_KEY).toString().split(","))
+                        .map(SimpleGrantedAuthority::new)
+                        .collect(Collectors.toList());
+
+        // 권한 정보를 담은 User객체 생성
+        User principal = new User(claims.getSubject(), "", authorities);
+
+        return new UsernamePasswordAuthenticationToken(principal, token, authorities);
+    }
+
+
+    // 토큰의 유효성 검증, 토큰을 파싱하여 exception들을 캐치
+    public boolean validateToken(String token) {
+        try {
+            Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(token);
+            return true;
+        } catch (io.jsonwebtoken.security.SecurityException | MalformedJwtException e) {
+            logger.info("잘못된 JWT 서명입니다.");
+        } catch (ExpiredJwtException e) {
+            logger.info("만료된 JWT 토큰입니다.");
+        } catch (UnsupportedJwtException e) {
+            logger.info("지원되지 않는 JWT 토큰입니다.");
+        } catch (IllegalArgumentException e) {
+            logger.info("JWT 토큰이 잘못되었습니다.");
+        }
+        return false;
     }
 }
