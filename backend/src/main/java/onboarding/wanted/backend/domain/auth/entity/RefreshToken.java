4bfcceb261a93bbd11abc3b8ee6ef4c0a675da13
jhkim <wellsy001012@gmail.com>
2023-08-16 01:33:19 +0900

    refactor: redis 토큰 저장 value 수정

--- backend/src/main/java/onboarding/wanted/backend/domain/auth/entity/RefreshToken.java
+++ backend/src/main/java/onboarding/wanted/backend/domain/auth/entity/RefreshToken.java
@@ -1,26 +1,18 @@
 package onboarding.wanted.backend.domain.auth.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.redis.core.RedisHash;
 
-@RedisHash(value = "refreshToken", timeToLive = 60)
-
+@Getter
+@AllArgsConstructor
+@RedisHash(value = "refreshToken", timeToLive = 60) // TODO: 테스트를 위해 짧은 시간 설정
 public class RefreshToken {
 
     @Id
+    @Getter
     private String refreshToken;
-    private String email;
 
-    public RefreshToken(final String refreshToken, final String email) {
-        this.refreshToken = refreshToken;
-        this.email = email;
-    }
-
-    public String getRefreshToken() {
-        return refreshToken;
-    }
-
-    public String getUserEmail() {
-        return email;
-    }
+    // 이후 redis에 저장되는 정보 추가 예정
 }
