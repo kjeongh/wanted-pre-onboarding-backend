9c981265d5fb284612fd4fd064e129a3f7344abe
jhkim <wellsy001012@gmail.com>
2023-08-15 18:55:33 +0900

    feat: 에러코드 정의

--- backend/src/main/java/onboarding/wanted/backend/global/error/ErrorCode.java
+++ backend/src/main/java/onboarding/wanted/backend/global/error/ErrorCode.java
@@ -11,7 +11,8 @@ public enum ErrorCode {
     //AUTH
     INVALID_JWT_SIGN(HttpStatus.UNAUTHORIZED, "잘못된 JWT 서명입니다."),
     EXPIRED_JWT_TOKEN(HttpStatus.UNAUTHORIZED, "만료된 JWT 토큰입니다."),
-    UNSUPPORTED_JWT_TOKEN(HttpStatus.UNAUTHORIZED, "지원되지 않는 JWT 토큰입니다.");
+    UNSUPPORTED_JWT_TOKEN(HttpStatus.UNAUTHORIZED, "지원되지 않는 JWT 토큰입니다."),
+    EXPIRED_REFRESH_TOKEN(HttpStatus.UNAUTHORIZED, "만료된 refresh 토큰입니다.");
 
     private final HttpStatus status;
     private final String message;
